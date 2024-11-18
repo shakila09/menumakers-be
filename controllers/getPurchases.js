19d269c117e118c34421bf7fa8e8b4052f4288c8
Pinki <pinkymaan1230@gmail.com>
2024-11-18 15:25:45 -0500

    implemented redis on fetching purchased templates by the user

--- controllers/getPurchases.js
+++ controllers/getPurchases.js
@@ -1,5 +1,5 @@
 const Purchase = require('../models/Purchase');
-
+const redisClient = require('../redisClient'); // Import Redis client
 exports.getUserPurchases = async (req, res) => {
   const { userEmail } = req.query;
 
@@ -8,7 +8,20 @@ exports.getUserPurchases = async (req, res) => {
   }
 
   try {
-    const purchases = await Purchase.find({ userEmail });
+
+      const cacheKey = `purchases:${userEmail}`;
+      
+      // Check if data exists in Redis cache
+      const cachedData = await redisClient.get(cacheKey);
+      if (cachedData) {
+        console.log('Serving from Redis cache');
+        return res.json(JSON.parse(cachedData));
+      }
+
+    const purchases = await Purchase.find({ userEmail }).lean();
+     // Cache the result in Redis for 1 hour (3600 seconds)
+    await redisClient.setEx(cacheKey, 3600, JSON.stringify(purchases));
+    console.log('Serving from MongoDB and caching the result');
     res.status(200).json(purchases);
   } catch (error) {
     console.error('Error fetching purchases:', error);
