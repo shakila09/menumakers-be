cef5dd23518fa500ef023b68ec657597e37d19ef
shakila09 <shakilakapadia09@gmail.com>
2024-11-19 14:24:24 -0500

    MM-122

--- server.js
+++ server.js
@@ -6,13 +6,9 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const authRoutes = require('./routes/authRoutes');
 const paymentRoutes = require('./routes/paymentRoutes');
-<<<<<<< Updated upstream
 const purchaseRoutes = require('./routes/purchaseRoutes');
-=======
-const saveSvgRoute = require('./routes/saveSvgRoute');
 const templateRoutes = require('./routes/templateRoutes');
 
->>>>>>> Stashed changes
 const app = express();
 
 // Load environment variables from .env file
@@ -69,16 +65,17 @@ app.get('/api/auth/session', (req, res) => {
 // Routes
 app.use('/api/auth', authRoutes); // path matches your route folder structure
 app.use('/api/payment', paymentRoutes); // Use the payment route
-<<<<<<< Updated upstream
 app.use('/api/purchases', purchaseRoutes); // Adds purchase routes
-
-=======
-app.use('/api', saveSvgRoute);
 app.use('/api/templates', templateRoutes);
->>>>>>> Stashed changes
+
+
 
 // Start server
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+});
+
+app.use((req, res, next) => {
+  res.status(404).json({ success: false, message: 'Route not found' });
 });
